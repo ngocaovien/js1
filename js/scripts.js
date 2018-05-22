9e9a602f804c40f9efa9b48f3c740633573d6657
ngocaovien <ngocaovien@gmail.com>
2018-05-21 21:02:17 -0700

    first variable written

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var myName = "Vien";
+
+console.log(myName);
+
+
+// var //reserve word
+
+// myName //case-sensitive and should be in camelCasing
+
+ // var myName => function declaration
+ // = "name" => function definition
